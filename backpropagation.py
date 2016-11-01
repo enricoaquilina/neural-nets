906c197fea41f16ce942f9b6f1e30fb35f72cacb
enricoaquilina <tappiera00@gmail.com>
2016-11-01 14:50:28 +0100

    finished epoch method, to add momentum next

--- backpropagation.py
+++ backpropagation.py
@@ -22,7 +22,7 @@ class BackPropagationNetwork:
 
         #create the weight arrays(l1+1 because of the bias term)
         for (l1,l2) in zip(layerSize[:-1], layerSize[1:]):
-            self.weights.append(np.random.normal(scale=0.01, size=(l2, l1+1)))
+            self.weights.append(np.random.normal(scale=0.1, size=(l2, l1+1)))
 
     #Run method
     def run(self, input):
@@ -53,6 +53,45 @@ class BackPropagationNetwork:
 
         return self._layerOutput[-1].T
 
+    def train_epoch(self, input, target, trainingRate=0.2):
+        #this method trains for one epoch
+
+        deltas = [];
+        InputCases = input.shape[0]
+
+        #First run the network
+        self.run(input)
+
+        for layer in reversed(range(self.layerCount)):
+            if layer == self.layerCount - 1:
+                output_delta = self._layerOutput[layer] - target.T
+                error = np.sum(output_delta ** 2)
+                deltas.append(output_delta * self.sigmoid(self._layerInput[layer], True))
+            else:
+                #Sum(k∈K) = δk . Wjk
+                delta_pullback = self.weights[layer + 1].T * deltas[-1]
+                # Oj(1 − Oj)
+                derivative = self.sigmoid(self._layerInputs[layer], True)
+                #this is to remove the bias from the deltas which is in the last row
+                deltas.append(delta_pullback[:-1,:] * derivative)
+
+        #Compute the weight deltas
+        for index in range(self.layerCount):
+            delta_index = self.layerCount - 1 - index
+
+            if index == 0:
+                layerOutput = np.vstack([input.T, np.ones([1, InputCases])])
+            else:
+                layerOutput = np.vstack([self._layerOutput[index-1], np.ones([1, self._layerOutput[index - 1].shape[1]])])
+
+            weightDelta = np.sum(
+                             layerOutput[None, :, :].transpose(2, 0, 1) * deltas[delta_index][None, :, :].transpose(2, 1, 0)
+                             , axis = 0)
+
+            self.weights[index] -= trainingRate * weightDelta
+
+        return error
+
     #Transfer function
     def sigmoid(self, x, Derivative=False):
         if not Derivative:
@@ -63,7 +102,9 @@ class BackPropagationNetwork:
 
 if __name__ == "__main__":
     bpn = BackPropagationNetwork((2,2,1))
-    print(bpn.layerCount)
-    print(bpn.shape)
+    # print(bpn.layerCount)
     # print(bpn.weights[0])
+    print(bpn.weights[0].T)
+    print(bpn.shape)
     print(bpn.weights)
+
